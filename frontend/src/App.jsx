41dfb84d4e32d0ae778425006b3566eb97f4bc23
Emi004 <aenoaeiemi49@gmail.com>
2026-03-26 20:13:51 +0200

    made working sidebar

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,44 +1,54 @@
-import './App.css';
-import MediaPlayer from './components/MediaPlayer.jsx';
-import Navbar from './components/navbar/Navbar.jsx';
-import { useAudio } from './context/AudioContext.jsx';
+import "./App.css";
+import MediaPlayer from "./components/MediaPlayer.jsx";
+import Navbar from "./components/navbar/Navbar.jsx";
+import { useAudio } from "./context/AudioContext.jsx";
 //import for mock track, will be removed when real data is integrated from backend
-import violentCrimesArt from './assets/imgs/violent_crimes.webp';
-import violentCrimesAudio from './assets/audio/violent_crimes.mp3';
+import violentCrimesArt from "./assets/imgs/violent_crimes.webp";
+import violentCrimesAudio from "./assets/audio/violent_crimes.mp3";
+import Sidebar from "./components/Sidebar.jsx";
 
 function App() {
-	const { playTrack } = useAudio();
+  const { playTrack } = useAudio();
 
-	//mock track for testing player logic, will be replaced with real data from backend later
-	const testTrack = {
-		id: 1,
-		title: 'Violent Crimes',
-		artist: 'Kanye West',
-		imageUrl: violentCrimesArt,
-		url: violentCrimesAudio,
-	};
+  const testTrack = {
+    id: 1,
+    title: "Violent Crimes",
+    artist: "Kanye West",
+    imageUrl: violentCrimesArt,
+    url: violentCrimesAudio,
+  };
 
-	return (
-		<div className="min-h-screen bg-base-100 flex flex-col">
-  			<Navbar />
+  return (
+    <div className="h-screen flex flex-col overflow-hidden bg-base-100">
+      
+      <Navbar />
 
-			<main className="flex-1 flex flex-col items-center justify-center p-10 text-center">
-				<h1 className="text-3xl font-bold text-white">Welcome to Sonata</h1>
-				<p className="mt-4 text-white/70">
-					Click below to test the player logic.
-				</p>
+      <div className="flex-1 overflow-hidden">
+        <Sidebar>
+          <div className="h-full flex flex-col justify-between">
+            
+            <main className="flex-1 overflow-y-auto p-10 text-center ">
+              <h1 className="text-3xl font-bold text-white">Welcome to Sonata</h1>
+              <p className="mt-4 text-white/70">
+                Click below to test the player logic.
+              </p>
 
-				<button
-					className="btn btn-primary mt-6"
-					onClick={() => playTrack(testTrack)}
-				>
-					Play Test Track
-				</button>
-			</main>
+              <button
+                className="btn btn-primary mt-6"
+                onClick={() => playTrack(testTrack)}
+              >
+                Play Test Track
+              </button>
+              
+              <div className="h-[200vh]"></div> 
+            </main>
 
-			<MediaPlayer />
-		</div>
-	);
+            <MediaPlayer />
+          </div>
+        </Sidebar>
+      </div>
+    </div>
+  );
 }
 
 export default App;
---
+++ frontend/src/components/Sidebar.jsx
@@ -0,0 +1,152 @@
+import { useAuth } from "../context/AuthContext";
+import { NavLink } from "react-router-dom";
+
+function Sidebar({ children }) {
+  const { user } = useAuth();
+
+  return (
+    <div className="drawer lg:drawer-open h-full">
+      <input id="my-drawer-4" type="checkbox" className="drawer-toggle" />
+
+      {/* Main Content Area */}
+      <div className="drawer-content flex flex-col h-full overflow-hidden">
+        {children}
+      </div>
+
+      {/* Sidebar Area */}
+      <div className="drawer-side h-full z-40 is-drawer-close:overflow-visible">
+        <label
+          htmlFor="my-drawer-4"
+          aria-label="close sidebar"
+          className="drawer-overlay"
+        ></label>
+
+        {/* Inner Container: This is what justify-between acts on */}
+        <div className="flex h-full flex-col justify-between bg-base-100 border-r border-base-200 is-drawer-close:w-14 is-drawer-open:w-64 transition-all duration-300">
+          {/* TOP MENU */}
+          <ul className="menu w-full p-2 gap-1">
+            <li>
+              <button
+                className="is-drawer-close:tooltip is-drawer-close:tooltip-right"
+                data-tip="Menu"
+              >
+                <label htmlFor="my-drawer-4" className="cursor-pointer">
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    className="my-1.5 inline-block size-6"
+                    strokeWidth="2"
+                    stroke="currentColor"
+                  >
+                    <path
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      d="M4 6h16M4 12h16M4 18h16"
+                    ></path>
+                  </svg>
+                </label>
+                <span className="is-drawer-close:hidden font-medium">Menu</span>
+              </button>
+            </li>
+            <li>
+              <NavLink
+                to="/"
+                className="is-drawer-close:tooltip is-drawer-close:tooltip-right"
+                data-tip="Homepage"
+              >
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  viewBox="0 0 24 24"
+                  strokeWidth="2"
+                  fill="none"
+                  stroke="currentColor"
+                  className="my-1.5 inline-block size-6"
+                >
+                  <path d="M15 21v-8a1 1 0 0 0-1-1h-4a1 1 0 0 0-1 1v8"></path>
+                  <path d="M3 10a2 2 0 0 1 .709-1.528l7-5.999a2 2 0 0 1 2.582 0l7 5.999A2 2 0 0 1 21 10v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2z"></path>
+                </svg>
+                <span className="is-drawer-close:hidden">Homepage</span>
+              </NavLink>
+            </li>
+            <li>
+              <NavLink
+                to="/settings"
+                className="is-drawer-close:tooltip is-drawer-close:tooltip-right"
+                data-tip="Settings"
+              >
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  viewBox="0 0 24 24"
+                  strokeWidth="2"
+                  fill="none"
+                  stroke="currentColor"
+                  className="my-1.5 inline-block size-6"
+                >
+                  <path d="M20 7h-9"></path>
+                  <path d="M14 17H5"></path>
+                  <circle cx="17" cy="17" r="3"></circle>
+                  <circle cx="7" cy="7" r="3"></circle>
+                </svg>
+                <span className="is-drawer-close:hidden">Settings</span>
+              </NavLink>
+            </li>
+          </ul>
+
+          {/* BOTTOM MENU (Admin/Artist) */}
+          <ul className="menu w-full p-2  gap-1 pb-4">
+            {(user?.is_admin || user?.is_artist) && (
+              <li>
+                <NavLink
+                  to="/artist"
+                  className="is-drawer-close:tooltip is-drawer-close:tooltip-right"
+                  data-tip="Upload Music"
+                >
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    viewBox="0 0 16 16"
+                    fill="currentColor"
+                    className="my-1.5 inline-block size-6"
+                  >
+                    <path d="M11 6.64a1 1 0 0 0-1.243-.97l-1 .25A1 1 0 0 0 8 6.89v4.306A2.6 2.6 0 0 0 7 11c-.5 0-.974.134-1.338.377-.36.24-.662.628-.662 1.123s.301.883.662 1.123c.364.243.839.377 1.338.377s.974-.134 1.338-.377c.36-.24.662-.628.662-1.123V8.89l2-.5z" />
+                    <path d="M14 14V4.5L9.5 0H4a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h8a2 2 0 0 0 2-2M9.5 3A1.5 1.5 0 0 0 11 4.5h2V14a1 1 0 0 1-1 1H4a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1h5.5z" />
+                  </svg>
+                  <span className="is-drawer-close:hidden text-xs uppercase tracking-widest opacity-70">
+                    Upload Music
+                  </span>
+                </NavLink>
+              </li>
+            )}
+            {user?.is_admin && (
+              <li>
+                <NavLink
+                  to="/admin"
+                  className="is-drawer-close:tooltip is-drawer-close:tooltip-right"
+                  data-tip="Admin Dashboard"
+                >
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    width="16"
+                    height="16"
+                    fill="currentColor"
+                    class="bi bi-database-gear"
+                    viewBox="0 0 16 16"
+                    className="my-1.5 inline-block size-6"
+                  >
+                    <path d="M12.096 6.223A5 5 0 0 0 13 5.698V7c0 .289-.213.654-.753 1.007a4.5 4.5 0 0 1 1.753.25V4c0-1.007-.875-1.755-1.904-2.223C11.022 1.289 9.573 1 8 1s-3.022.289-4.096.777C2.875 2.245 2 2.993 2 4v9c0 1.007.875 1.755 1.904 2.223C4.978 15.71 6.427 16 8 16c.536 0 1.058-.034 1.555-.097a4.5 4.5 0 0 1-.813-.927Q8.378 15 8 15c-1.464 0-2.766-.27-3.682-.687C3.356 13.875 3 13.373 3 13v-1.302c.271.202.58.378.904.525C4.978 12.71 6.427 13 8 13h.027a4.6 4.6 0 0 1 0-1H8c-1.464 0-2.766-.27-3.682-.687C3.356 10.875 3 10.373 3 10V8.698c.271.202.58.378.904.525C4.978 9.71 6.427 10 8 10q.393 0 .774-.024a4.5 4.5 0 0 1 1.102-1.132C9.298 8.944 8.666 9 8 9c-1.464 0-2.766-.27-3.682-.687C3.356 7.875 3 7.373 3 7V5.698c.271.202.58.378.904.525C4.978 6.711 6.427 7 8 7s3.022-.289 4.096-.777M3 4c0-.374.356-.875 1.318-1.313C5.234 2.271 6.536 2 8 2s2.766.27 3.682.687C12.644 3.125 13 3.627 13 4c0 .374-.356.875-1.318 1.313C10.766 5.729 9.464 6 8 6s-2.766-.27-3.682-.687C3.356 4.875 3 4.373 3 4" />
+                    <path d="M11.886 9.46c.18-.613 1.048-.613 1.229 0l.043.148a.64.64 0 0 0 .921.382l.136-.074c.561-.306 1.175.308.87.869l-.075.136a.64.64 0 0 0 .382.92l.149.045c.612.18.612 1.048 0 1.229l-.15.043a.64.64 0 0 0-.38.921l.074.136c.305.561-.309 1.175-.87.87l-.136-.075a.64.64 0 0 0-.92.382l-.045.149c-.18.612-1.048.612-1.229 0l-.043-.15a.64.64 0 0 0-.921-.38l-.136.074c-.561.305-1.175-.309-.87-.87l.075-.136a.64.64 0 0 0-.382-.92l-.148-.045c-.613-.18-.613-1.048 0-1.229l.148-.043a.64.64 0 0 0 .382-.921l-.074-.136c-.306-.561.308-1.175.869-.87l.136.075a.64.64 0 0 0 .92-.382zM14 12.5a1.5 1.5 0 1 1-3 0 1.5 1.5 0 0 1 3 0" />
+                  </svg>
+                  <span className="is-drawer-close:hidden text-xs uppercase tracking-widest opacity-70">
+                    Admin Dashboard
+                  </span>
+                </NavLink>
+              </li>
+            )}
+          </ul>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Sidebar;
